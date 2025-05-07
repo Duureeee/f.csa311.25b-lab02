076d15155eb7c06b5c41fdb209de80ea3c73d3b0
Duureeee <167115601+Duureeee@users.noreply.github.com>
2025-05-08 05:01:23 +0800

    а

---
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Main.java
@@ -0,0 +1,16 @@
+// Main.java
+package edu.cmu.cs.cs214.lab02;
+
+import edu.cmu.cs.cs214.lab02.shapes.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Shape rectangle = new Rectangle(2, 3);
+        Shape circle = new Circle(5);
+        Shape square = new Square(4);
+
+        new Renderer(rectangle).draw();
+        new Renderer(circle).draw();
+        new Renderer(square).draw();
+    }
+}
